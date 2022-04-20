2ad09e59c08b9f8b0f9d09cd4e9752afd4919b90
Pavel Egorov <egorov.p@gmail.com>
2022-04-20 17:15:01 +0300

    Add project files.

---
+++ SimpleFlatUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using SimpleFlatUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFlatUI.MVVM.ViewModel
+{
+    
+    internal class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
